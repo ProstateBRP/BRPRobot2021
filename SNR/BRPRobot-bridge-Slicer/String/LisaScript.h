6e06e75b4c12d353dd8175ae4b878b1c113efb31
Rebecca Lisk <31640989+rebeccalisk@users.noreply.github.com>
2021-10-20 15:15:51 -0400

    Deleted startup function

--- SNR/BRPRobot-bridge-Slicer/String/LisaScript.h
+++ SNR/BRPRobot-bridge-Slicer/String/LisaScript.h
@@ -4,13 +4,11 @@
 
 // prototype function declarations
 void SendStringToSlicer(char*  hostname, int port, char* argDeviceName,char* argMessage);
-void SendStateToSlicer(char*  hostname, int port, char* argDeviceName, unsigned short argCode, unsigned  long  long argSubcode, char* argErrorName, char* argStatusStringMessage);
+void SendStatusToSlicer(char*  hostname, int port, char* argDeviceName, unsigned short argCode, unsigned  long  long argSubcode, char* argErrorName, char* argStatusStringMessage);
 void SendTransformToSlicer(const char* hostname, int port, char* argDeviceName, igtl::Matrix4x4& matrix);
 
-void startup();
 int getTransform();
 int getStatus();
 
 
 #endif
-
