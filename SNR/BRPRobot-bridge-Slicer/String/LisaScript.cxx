218e54c3dd95b2a23d819370a0af6d5d5efc41f9
Rebecca Lisk <31640989+rebeccalisk@users.noreply.github.com>
2021-10-20 08:52:43 -0400

    Add files via upload

---
+++ SNR/BRPRobot-bridge-Slicer/String/LisaScript.cxx
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+
+#include "LisaScript.h"
+
+void startup()
+{
+	int a;
+}
+
+int getTransform()
+{
+	return 1;
+}
+
+int getStatus()
+{
+	return 2;
+}
---
+++ SNR/BRPRobot-bridge-Slicer/String/LisaScript.h
@@ -0,0 +1,11 @@
+// LisaScript.h
+#ifndef _LISA_SCRIPT
+#define _LISA_SCRIPT
+
+// prototype function declarations
+void startup();
+int getTransform();
+int getStatus();
+
+#endif
+
